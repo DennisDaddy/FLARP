2efa664db00d9d6c184473821aad9f6e43b2c665
DennisDaddy <softteez95@gmail.com>
2018-06-01 08:49:33 +0300

    Initial Commit

---
+++ data.py
@@ -0,0 +1,33 @@
+def Articles():
+ 	articles = [
+ 	{
+       'id': 1,
+       'title': 'Article One' ,
+       'body': 'Bootstrap is not supported in the old Internet Explorer compatibility modes',
+       'author': 'Dennis Kings',
+       'create_date': '1/15/2015'
+
+       },
+
+       {
+       'id': 2,
+       'title': 'Article Two',
+       'body': 'Bootstrap is not supported in the old Internet Explorer compatibility modes',
+       'author': 'Moracha Kings',
+       'create_date': '6/15/2015'
+
+       },
+
+        {
+       'id': 3,
+       'title': 'Article Three',
+       'body': 'Life is not supported in the old Internet Explorer compatibility modes',
+       'author': 'Caro Big head',
+       'create_date': '5/8/2019'
+
+       }
+
+
+ 	]
+
+ 	return articles
